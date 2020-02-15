b53d95fa69df456f3ed7f34101934840eaabd57d
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-14 21:31:23 -0300

    Commit inicial do repositório

---
+++ TreinaWeb.Musicas.Dominio/Dominio/Album.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TreinaWeb.Musicas.Dominio.Dominio
+{
+    /// <summary>
+    /// Classe responsável por representar o domínio Album.
+    /// </summary>
+    public class Album
+    {
+        /// <value>Implementa o id do álbum.</value>
+        public int Id { get; set; }
+
+        /// <value>Representa o nome do álbum.</value>
+        public string Nome { get; set; }
+
+        /// <value>Imforma o ano de lançamento do álbum.</value>
+        public int Ano { get; set; }
+
+        /// <value>Atribui um email ao álbum.</value>
+        public string Email { get; set; }
+
+        /// <value>Imforma qualquer detalhe referente álbum.</value>
+        public string Observacoes { get; set; }
+
+        /// <value>Exibe uma lista de músicas.</value>
+        public virtual List<Musica> Musicas { get; set; }
+    }
+}
---
+++ TreinaWeb.Musicas.Dominio/Dominio/Musica.cs
@@ -0,0 +1,20 @@
+﻿namespace TreinaWeb.Musicas.Dominio.Dominio
+{
+    /// <summary>
+    /// Classe responsável por representar o domínio música.
+    /// </summary>
+    public class Musica
+    {
+        /// <value>Implementa o id da música.</value>
+        public long Id { get; set; }
+
+        /// <value>Representa o nome da música.</value>
+        public string Nome { get; set; }
+
+        /// <value>Responsável pela declaração da música a um álbum.</value>
+        public virtual Album Album { get; set; }
+
+        /// <value>Atribui uma música a um álbum.</value>
+        public int IdAlbum { get; set; }
+    }
+}
