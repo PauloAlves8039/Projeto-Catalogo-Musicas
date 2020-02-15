03a7a6d4355f504c129d01e0cb769926e0e4c25e
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-14 22:26:25 -0300

    Classe para uso do contexto do Entity Framework

---
+++ TreinaWeb.Musicas.AcessoDados.Entity/Context/MusicasDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using TreinaWeb.Musicas.Dominio.Dominio;
+
+namespace TreinaWeb.Musicas.AcessoDados.Entity.Context
+{
+    /// <summary>
+    /// Classe responsável por herda contexto da classe DbContext para uso do Entity Framework no mapeamento do banco de dados.
+    /// </summary>
+    public class MusicasDbContext : DbContext
+    {
+        /// <value>Representa o domínio Album como uma tabela no banco de dados através do DbSet.</value>
+        public DbSet<Album> Albuns { get; set; }
+
+        /// <value>Representa o domínio Musica como uma tabela no banco de dados através do DbSet.</value>
+        public DbSet<Musica> Musicas { get; set; }
+    }
+}
