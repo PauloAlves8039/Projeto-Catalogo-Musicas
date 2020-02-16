42c50992bc0ae80f133a97eb116e9d444855a227
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-15 23:21:18 -0300

    Classe de template para configuração com o banco de dados

---
+++ TreinaWeb.Musicas.Comum.Entity/Abstract/TreinaWebEntityAbstractConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace TreinaWeb.Musicas.Comum.Entity.Abstract
+{
+    /// <summary>
+    /// Classe de template genérica e abstrata, contém métodos responsáveis pelas configurações das entidades no banco de dados.
+    /// </summary>
+    public abstract class TreinaWebEntityAbstractConfig<TEntidade> : EntityTypeConfiguration<TEntidade>
+        where TEntidade : class
+    {
+        /// <summary>
+        /// Construtor para definir sequência de métodos genéricos na criação de tabelas no banco de dados.
+        /// </summary>
+        public TreinaWebEntityAbstractConfig()
+        {
+            ConfigurarNomeTabela();
+            ConfigurarCampoTabela();
+            ConfigurarChavePrimaria();
+            ConfigurarChavesEstrangeiras();
+        }
+
+        /// <summary>
+        /// Método abstrato para configurar chave estrangeira.
+        /// </summary>
+        protected abstract void ConfigurarChavesEstrangeiras();
+
+        /// <summary>
+        /// Método abstrato para configurar chave primaria.
+        /// </summary>
+        protected abstract void ConfigurarChavePrimaria();
+
+        /// <summary>
+        /// Método abstrato para configurar campos da tabela.
+        /// </summary>
+        protected abstract void ConfigurarCampoTabela();
+
+        /// <summary>
+        /// Método abstrato para configurar o nome da tabela.
+        /// </summary>
+        protected abstract void ConfigurarNomeTabela();
+    }
+}
