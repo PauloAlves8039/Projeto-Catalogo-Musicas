2c95779e7cd628cf7041555e556f051ded9d9310
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-16 22:36:52 -0300

    Definindo configuração das entidades no banco de dados

--- TreinaWeb.Musicas.AcessoDados.Entity/Context/MusicasDbContext.cs
+++ TreinaWeb.Musicas.AcessoDados.Entity/Context/MusicasDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TreinaWeb.Musicas.AcessoDados.Entity.TypeConfiguration;
 using TreinaWeb.Musicas.Dominio.Dominio;
 
 namespace TreinaWeb.Musicas.AcessoDados.Entity.Context
@@ -13,5 +15,29 @@ namespace TreinaWeb.Musicas.AcessoDados.Entity.Context
 
         /// <value>Representa o domínio Musica como uma tabela no banco de dados através do DbSet.</value>
         public DbSet<Musica> Musicas { get; set; }
+
+        /// <summary>
+        /// Construtor sobrescrito para configurar o carregamento de dados
+        /// </summary>
+        public MusicasDbContext()
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        /// <summary>
+        /// Método sobrescrito para configuração dos modelos das entidades no banco de dados.
+        /// </summary>
+        /// <param name="modelBuilder">Propriedade responsável pela criação de relação entre as entidades no banco de dados.</param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AlbumTypeConfiguration());
+            modelBuilder.Configurations.Add(new MusicaTypeConfiguration());
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Properties()
+                .Configure(p => p.HasColumnType("varhcar"));
+        }
     }
 }
