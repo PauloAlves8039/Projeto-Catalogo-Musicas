3401c581574d3e4de673307e129f16a9e6a4ff7b
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-18 21:17:57 -0300

    Adicionando classes de modelo para iteração com as views

---
+++ TreinaWeb.Musicas.Web/ViewModels/Album/AlbumIndexViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TreinaWeb.Musicas.Web.ViewModels.Album
+{
+    /// <summary>
+    /// Classe de modelo usada para iteração de suas propriedades com a view Albuns, utilizando Data Annotations para exibição dessas propriedades.
+    /// </summary>
+    public class AlbumIndexViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nome do álbum")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Ano do álbum")]
+        public int Ano { get; set; }
+
+        [Display(Name = "Email de contato")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Observações")]
+        public string Observacoes { get; set; }
+    }
+}
---
+++ TreinaWeb.Musicas.Web/ViewModels/Musica/MusicaIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TreinaWeb.Musicas.Web.ViewModels.Musica
+{
+    /// <summary>
+    /// Classe de modelo usada para iteração de suas propriedades com a view Musicas, utilizando Data Annotations para exibição dessas propriedades.
+    /// </summary>
+    public class MusicaIndexViewModel
+    {
+        [Required(ErrorMessage = "O Id é obrigatório!")]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório!")]
+        [MaxLength(100, ErrorMessage = "O nome da música pode ter no máximo 100 caractereres!")]
+        [Display(Name = "Nome da música!")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Álbum")]
+        public string IdAlbum { get; set; }
+    }
+}
