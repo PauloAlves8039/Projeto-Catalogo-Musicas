121488302db503382aa4e08b74c1dfedddeee01c
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-21 19:49:33 -0300

    Adicionando classes de repositórios das entidades

---
+++ TreinaWeb.Musicas.Repositorios.Entity/Entidades/AlbunsRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using TreinaWeb.Musicas.AcessoDados.Entity.Context;
+using TreinaWeb.Musicas.Dominio.Dominio;
+using TreinaWeb.Repositorios.Comum.Entity.Repositorio;
+
+namespace TreinaWeb.Musicas.Repositorios.Entity.Entidades
+{
+    /// <summary>
+    /// Classe que representa repositório da entidade Album, herdando métodos da classe RepositorioGenericoEntity.
+    /// </summary>
+    public class AlbunsRepositorio : RepositorioGenericoEntity<Album, int>
+    {
+        /// <summary>
+        /// Construtor sobrescrito para uso do DbContext.
+        /// </summary>
+        /// <param name="contexto">Parâmetro de referência para uso do DbContext.</param>
+        public AlbunsRepositorio(MusicasDbContext contexto)
+            : base(contexto)
+        {
+
+        }
+
+
+    }
+}
---
+++ TreinaWeb.Musicas.Repositorios.Entity/Entidades/MusicasRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using TreinaWeb.Musicas.AcessoDados.Entity.Context;
+using TreinaWeb.Musicas.Dominio.Dominio;
+using TreinaWeb.Repositorios.Comum.Entity.Repositorio;
+
+namespace TreinaWeb.Musicas.Repositorios.Entity.Entidades
+{
+    /// <summary>
+    /// Classe que representa repositório da entidade Musica, herdando métodos da classe RepositorioGenericoEntity.
+    /// </summary>
+    public class MusicasRepositorio : RepositorioGenericoEntity<Musica, long>
+    {
+        /// <summary>
+        /// Construtor sobrescrito para uso do DbContext.
+        /// </summary>
+        /// <param name="contexto">Parâmetro de referência para uso do DbContext.</param>
+        public MusicasRepositorio(MusicasDbContext contexto)
+            : base (contexto)
+        {
+
+        }
+    }
+}
